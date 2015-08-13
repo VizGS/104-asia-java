bb48530ca17c0babcbb35801db36d912b83225c7
VizGS <104021020@live.asia.edu.tw>
2015-08-13 16:08:11 +0800

    Java

---
+++ 陣列Array 20150811主題7/src/Java5.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+public class Java5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Random a=new Random();
+		
+	}
+
+}
