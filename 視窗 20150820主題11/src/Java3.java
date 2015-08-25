3649035e58906f8127d51bcab79f0b857ce2538a
VizGS <104021020@live.asia.edu.tw>
2015-08-25 16:12:42 +0800

    Java

--- 視窗 20150820主題11/src/Java3.java
+++ 視窗 20150820主題11/src/Java3.java
@@ -2,165 +2,182 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
-public class Java3 extends JFrame  {
 
-	private static JTextField Fuck=new JTextField("0");
-	private JPanel Fuckyou=new JPanel();
-    private JPanel Fuckyou2=new JPanel();
-    private JPanel Fuckyou3=new JPanel();
+public class Java3 extends JFrame {
+
+	private static JTextField Fuck = new JTextField("0");
+	private JPanel Fuckyou = new JPanel();
+	private JPanel Fuckyou2 = new JPanel();
+	private JPanel Fuckyou3 = new JPanel();
 	private Container ass;
-	private JButton cn,a,b,c,d,e,f,g,h, i,  j,  k, l,  m,  n,  o,  p ,q;
-	               //0,%,+,-,*,/,←,√,(,x2,log,sin,cos,tan,cot,sec,csc,)
-    private  long num;
-    private  byte op;
-	private JButton ab[]=new JButton[12];
-	public Java3(){
+	private static long num;
+	private static byte op;
+	private static int x;
+	private static JButton ab[] = new JButton[20];
+	private static JButton ab2[] = new JButton[15];
+	// Button
+	String str[] = { "←", "√", "C", "±", "7", "8", "9", "×", "4", "5", "6",
+			"÷", "1", "2", "3", "-", "0", ".", "=", "+" };
+	String str2[] = { "sinh", "(", ")", "cosh", "sin", "cos", "tanh", "tan",
+			"cot", "Exp", "sec", "csc", "Mod", "log", "x²", };
+
+	public Java3() {
 		yourmother();
 	}
-	private void yourmother(){
-		
-		ass=this.getContentPane();
+
+	private void yourmother() {
+
+		ass = this.getContentPane();
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setBounds(450,250,500,400);
-		ass.setLayout(new BorderLayout(5,5));
+		this.setBounds(450, 250, 600, 400);
+		ass.setLayout(new BorderLayout(5, 5));
+		// 顯示器螢幕
 		Fuck.setBackground(Color.pink);
 		Fuck.setEditable(false);
 		Fuck.setHorizontalAlignment(JTextField.RIGHT);
-		Fuck.setFont(new Font("Times New Roman",Font.BOLD,32));
-		ass.add(Fuck,BorderLayout.NORTH);
-		
-		Fuckyou.setLayout(new GridLayout(5,3,3,3));
-		ass.add(Fuckyou,BorderLayout.CENTER);
-		
-		f=new JButton("←");
-		Fuckyou.add(f);
-		f.setFont(new Font("Times New Roman",Font.BOLD,28));		
-		
-		g=new JButton("√");
-		Fuckyou.add(g);
-		g.setFont(new Font("Times New Roman",Font.BOLD,28));
-		g.addActionListener(new Java4());
-		
-		cn=new JButton("C");
-		Fuckyou.add(cn);
-		cn.setFont(new Font("Times New Roman",Font.BOLD,28));
-		cn.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent ae){
+		Fuck.setFont(new Font("Times New Roman", Font.BOLD, 32));
+		ass.add(Fuck, BorderLayout.NORTH);
+		// ass的Center中放入Fuckyou_GridLayout
+		Fuckyou.setLayout(new GridLayout(1, 2, 3, 3));
+		ass.add(Fuckyou, BorderLayout.CENTER);
+		// Fuckyou2放入Fuckyou的左盤
+		Fuckyou2.setLayout(new GridLayout(5, 2, 3, 3));
+		Fuckyou.add(Fuckyou2);
+		// Fuckyou3放入Fuckyou的右盤
+		Fuckyou3.setLayout(new GridLayout(5, 4, 3, 3));
+		Fuckyou.add(Fuckyou3);
+		// Fcukyou2_JButton的設定
+		for (int k = 0; k < 15; k++) {
+			ab[k] = new JButton(str2[k]);
+			ab[k].setOpaque(true);
+			ab[k].setHorizontalAlignment(JButton.CENTER);
+			ab[k].setFont(new Font("Times New Roman", Font.BOLD, 28));
+			ab[k].setBackground(Color.WHITE);
+			Fuckyou2.add(ab[k]);
+		}
+		ab[0].addActionListener(new Java4());
+		ab[1].addActionListener(new Java4());
+		// Fuckyou3_JButton的設定
+		for (int i = 0; i < 20; i++) {
+			ab[i] = new JButton(str[i]);
+			ab[i].setOpaque(true);
+			ab[i].setHorizontalAlignment(JButton.CENTER);
+			ab[i].setFont(new Font("Times New Roman", Font.BOLD, 28));
+			Fuckyou3.add(ab[i]);
+		}
+		//
+		if(x==1){
+
+		}
+		//CN的歸零
+		ab[2].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
 				Fuck.setText("0");
 			}
 		});
-		
-		for(int i=9;i>0;i--){
-			ab[i]=new JButton(Integer.toString(i));
-			ab[i].setOpaque(true);
-			ab[i].setHorizontalAlignment(JButton.CENTER);
-			ab[i].setFont(new Font("Times New Roman",Font.BOLD,28));
-			ab[i].addActionListener(new Java4());
-			Fuckyou.add(ab[i]);
+		//刪除倒退鍵
+		ab[0].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				String str=Fuck.getText();
+				String str2=str.substring(0,str.length()-1);
+				str=str2;
+				Fuck.setText(str);
+				if(Fuck.getText().equals("")){
+					Fuck.setText("0");	
+				}
+			}
+		});
+		//數字的正負
+		ab[3].addActionListener(new ActionListener() {
+			int a=0;
+			public void actionPerformed(ActionEvent ae) {
+				if(Fuck.getText().equals("0")){
+					a=2;
+				}
+				switch(a){
+				case 0:
+					String str3=(char)45+Fuck.getText();
+					Fuck.setText(str3);
+					a++;
+					break;
+				case 1:
+					String str=Fuck.getText();
+					String str2=str.substring(1,str.length());
+					str=str2;
+					Fuck.setText(str);
+					a--;
+					break;
+				case 2:
+					Fuck.setText("0");
+					a=0;
+					break;
+				}
+			}
+		});
+		//	    
+		for (int j = 4; j < 7; j++) {
+			ab[j].addActionListener(new Java4());
 		}
-		
-		ab[0]=new JButton(Integer.toString(0));
-		Fuckyou.add(ab[0]);
-		ab[0].setFont(new Font("Times New Roman",Font.BOLD,28));
-		ab[0].addActionListener(new Java4());
-		
-		ab[10]=new JButton("=");
-		Fuckyou.add(ab[10]);
-		ab[10].setFont(new Font("Times New Roman",Font.BOLD,28));
-		
-		ab[11]=new JButton(".");
-		Fuckyou.add(ab[11]);
-		ab[11].setFont(new Font("Times New Roman",Font.BOLD,28));
-		ab[11].addActionListener(new Java4());
-		
-		Fuckyou2.setLayout(new GridLayout(5,1,3,3));
-		ass.add(Fuckyou2,BorderLayout.EAST);
-		
-		a=new JButton("%");
-		Fuckyou2.add(a);
-		a.setFont(new Font("Times New Roman",Font.BOLD,25));
-		a.addActionListener(new Java4());
-		
-		b=new JButton("+");
-		Fuckyou2.add(b);
-		b.setFont(new Font("Times New Roman",Font.BOLD,25));
-		b.addActionListener(new Java4());
-		
-		c=new JButton("-");
-		Fuckyou2.add(c);
-		c.setFont(new Font("Times New Roman",Font.BOLD,25));
-		c.addActionListener(new Java4());
-		
-		d=new JButton("×");
-		Fuckyou2.add(d);
-		d.setFont(new Font("Times New Roman",Font.BOLD,25));
-		d.addActionListener(new Java4());
-		
-		e=new JButton("÷");
-		Fuckyou2.add(e);
-		e.setFont(new Font("Times New Roman",Font.BOLD,25));
-		e.addActionListener(new Java4());
-		
-		
-		Fuckyou3.setLayout(new GridLayout(5,2,3,3));
-		ass.add(Fuckyou3,BorderLayout.WEST);
-		
-		h=new JButton("(");
-		Fuckyou3.add(h);
-		h.setFont(new Font("Times New Roman",Font.BOLD,25));
-		h.addActionListener(new Java4());
-		
-		q=new JButton(")");
-		Fuckyou3.add(q);
-		q.setFont(new Font("Times New Roman",Font.BOLD,25));
-		q.addActionListener(new Java4());
-		
-		i=new JButton("x²");
-		Fuckyou3.add(i);
-		i.setFont(new Font("Times New Roman",Font.BOLD,25));
-				
-		j=new JButton("log");
-		Fuckyou3.add(j);
-    	j.setFont(new Font("Times New Roman",Font.BOLD,25));
-    	j.addActionListener(new Java4());
-    	
-		k=new JButton("sin");
-		Fuckyou3.add(k);
-		k.setFont(new Font("Times New Roman",Font.BOLD,25));
-		
-		l=new JButton("cos");
-		Fuckyou3.add(l);
-		l.setFont(new Font("Times New Roman",Font.BOLD,25));
-		
-		m=new JButton("tan");
-		Fuckyou3.add(m);
-		m.setFont(new Font("Times New Roman",Font.BOLD,25));
-		
-		n=new JButton("cot");
-		Fuckyou3.add(n);
-		n.setFont(new Font("Times New Roman",Font.BOLD,25));
-		
-		o=new JButton("sec");
-		Fuckyou3.add(o);
-		o.setFont(new Font("Times New Roman",Font.BOLD,25));
-		
-		p=new JButton("csc");
-		Fuckyou3.add(p);
-		p.setFont(new Font("Times New Roman",Font.BOLD,25));
-		
+		for (int m = 8; m < 11; m++) {
+			ab[m].addActionListener(new Java4());
+		}
+		for (int n = 12; n < 15; n++) {
+			ab[n].addActionListener(new Java4());
+		}
+		ab[16].addActionListener(new Java4());
+		ab[17].addActionListener(new Java4());
 	}
 
 	public static class Java4 implements ActionListener {
-		public void actionPerformed(ActionEvent ae){
-			JButton asshole=(JButton)ae.getSource();
-			if(Fuck.getText().equals("0")){
-			Fuck.setText("");
-		}
-		Fuck.setText(Fuck.getText()+asshole.getText());
-		
-	}
+		public void actionPerformed(ActionEvent ae) {
+			JButton asshole = (JButton) ae.getSource();
+			if(Fuck.getText().equals("0")&&asshole.getText()!="."){
+				Fuck.setText("");
+				Fuck.setText(Fuck.getText() + asshole.getText());
+			}else{
+				Fuck.setText(Fuck.getText() + asshole.getText());
+			}
 
-		
+//			String mustSplitString=Fuck.getText();
+//			String[] AfterSplit = mustSplitString.split("+ - × ÷");
+//			
+//			
+//			long abc;
+
+
+//			if (asshole == ab[19]) {
+//				op = 1;
+//			} else if (asshole == ab[15]) {
+//				op = 2;
+//			} else if (asshole == ab[11]) {
+//				op = 3;
+//			} else if (asshole == ab[7]) {
+//				op = 4;
+//			} else {
+//				if (asshole == ab[18]) {
+//					abc = Long.parseLong(Fuck.getText());
+//
+//					switch (op) {
+//
+//					case 1:
+//						num += abc;
+//						break;
+//					case 2:
+//						num -= abc;
+//						break;
+//					case 3:
+//						num *= abc;
+//						break;
+//					case 4:
+//						num /= abc;
+//						break;
+//					default:
+//						num = 0;
+//					}
+//					Fuck.setText(Long.toString(num));
+//				}
+//			}
 		}
+	}
 
 }
