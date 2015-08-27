a85d082330d5aadbbbd43a349cf5db486f7c2422
VizGS <104021020@live.asia.edu.tw>
2015-08-27 15:54:10 +0800

    Java

--- 視窗 20150820主題11/src/Java3.java
+++ 視窗 20150820主題11/src/Java3.java
@@ -10,16 +10,17 @@ public class Java3 extends JFrame {
 	private JPanel Fuckyou2 = new JPanel();
 	private JPanel Fuckyou3 = new JPanel();
 	private Container ass;
-	private static long num;
+	private static double num;
+	private static double abc;
 	private static byte op;
 	private static JButton ab[] = new JButton[20];
-	private static JButton ab2[] = new JButton[15];
+	private static JButton ab2[] = new JButton[12];
 	private static boolean dot = false;
 	// Button
 	String str[] = { "←", "√", "C", "±", "7", "8", "9", "×", "4", "5", "6",
 			"÷", "1", "2", "3", "-", "0", ".", "=", "+" };
-	String str2[] = { "sinh", "(", ")", "cosh", "sin", "cos", "tanh", "tan",
-			"cot", "Exp", "sec", "csc", "Mod", "log", "x²", };
+	String str2[] = { "sinh", "sin", "cos", "cosh", "tan", "cot", "tanh",
+			"sec", "csc", "x³", "log", "x²" };
 
 	public Java3() {
 		yourmother();
@@ -29,6 +30,7 @@ public class Java3 extends JFrame {
 
 		// 視窗設定
 		ass = this.getContentPane();
+		this.setTitle("工程計算機  Ver.0.1.0  by吳秉鴻");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setBounds(450, 250, 600, 400);
 		ass.setLayout(new BorderLayout(5, 5));
@@ -42,13 +44,13 @@ public class Java3 extends JFrame {
 		Fuckyou.setLayout(new GridLayout(1, 2, 3, 3));
 		ass.add(Fuckyou, BorderLayout.CENTER);
 		// Fuckyou2放入Fuckyou的左盤
-		Fuckyou2.setLayout(new GridLayout(5, 2, 3, 3));
+		Fuckyou2.setLayout(new GridLayout(4, 3, 3, 3));
 		Fuckyou.add(Fuckyou2);
 		// Fuckyou3放入Fuckyou的右盤
 		Fuckyou3.setLayout(new GridLayout(5, 4, 3, 3));
 		Fuckyou.add(Fuckyou3);
 		// Fcukyou2_JButton的設定
-		for (int k = 0; k < 15; k++) {
+		for (int k = 0; k < 12; k++) {
 			ab2[k] = new JButton(str2[k]);
 			ab2[k].setOpaque(true);
 			ab2[k].setHorizontalAlignment(JButton.CENTER);
@@ -65,10 +67,6 @@ public class Java3 extends JFrame {
 			ab[i].setHorizontalAlignment(JButton.CENTER);
 			ab[i].setFont(new Font("Times New Roman", Font.BOLD, 28));
 			Fuckyou3.add(ab[i]);
-		}
-		// Fuckyou3_JButton放入Java4
-		for (int j = 4; j < 20; j++) {
-			ab[j].addActionListener(new Java4());
 		}
 		// 小數點設定
 		ab[17].addActionListener(new ActionListener() {
@@ -100,6 +98,21 @@ public class Java3 extends JFrame {
 				}
 			}
 		});
+		// 根號
+		ab[1].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				num = Double.parseDouble(Fuck.getText());
+				double a = Math.sqrt(num);
+				String str = String.valueOf(a);
+				String[] data = str.split("\\.");
+				double n = Double.parseDouble(data[1]);
+				if (n == 0) {
+					Fuck.setText(data[0]);
+				} else {
+					Fuck.setText(Double.toString(a));
+				}
+			}
+		});
 		// 數字的正負
 		ab[3].addActionListener(new ActionListener() {
 			int a = 0;
@@ -128,74 +141,241 @@ public class Java3 extends JFrame {
 				}
 			}
 		});
+		// +,-,*,/
+		ab[19].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				if (!Fuck.getText().equals("")) {
+					op = 1;
+					num = Double.parseDouble(Fuck.getText());
+					Fuck.setText("");
+				}
+			}
+		});
+		ab[15].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				if (!Fuck.getText().equals("")) {
+					op = 2;
+					num = Double.parseDouble(Fuck.getText());
+					Fuck.setText("");
+				}
+			}
+		});
+		ab[11].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				if (!Fuck.getText().equals("")) {
+					op = 3;
+					num = Double.parseDouble(Fuck.getText());
+					Fuck.setText("");
+				}
+			}
+		});
+		ab[7].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				if (!Fuck.getText().equals("")) {
+					op = 4;
+					num = Double.parseDouble(Fuck.getText());
+					Fuck.setText("");
+				}
+			}
+		});
+		// sin
+		ab2[1].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				num = Double.parseDouble(Fuck.getText());
+				double a = Math.sin(num / (180 / Math.PI));
+				Fuck.setText(Double.toString(a));
+			}
+		});
+		// cos
+		ab2[2].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				num = Double.parseDouble(Fuck.getText());
+				if (num == 90) {
+					Fuck.setText("0.0");
+				} else {
+					double a = Math.cos(num / (180 / Math.PI));
+					Fuck.setText(Double.toString(a));
+				}
+			}
+		});
+		// tan
+		ab2[4].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				num = Double.parseDouble(Fuck.getText());
+				if (num == 90 || num == 270) {
+					Fuck.setText("No  answer");
+				} else if (num == 180) {
+					Fuck.setText("0");
+				} else {
+					double a = Math.tan(num / (180 / Math.PI));
+					Fuck.setText(Double.toString(a));
+				}
+			}
+
+		});
+		// cot
+		ab2[5].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				num = Double.parseDouble(Fuck.getText());
+				if (num == 90 || num == 270) {
+					Fuck.setText("No  answer");
+				} else if (num == 180) {
+					Fuck.setText("0");
+				} else {
+					double a = Math.tan(num / (180 / Math.PI));
+					Fuck.setText(Double.toString(1 / a));
+				}
+			}
+		});
+		// sec
+		ab2[7].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				num = Double.parseDouble(Fuck.getText());
+				if (num == 90) {
+					Fuck.setText("0.0");
+				} else {
+					double a = Math.cos(num / (180 / Math.PI));
+					Fuck.setText(Double.toString(1 / a));
+				}
+			}
+		});
+		// csc
+		ab2[8].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				num = Double.parseDouble(Fuck.getText());
+				double a = Math.sin(num / (180 / Math.PI));
+				Fuck.setText(Double.toString(1 / a));
+			}
+		});
+		// sinh
+		ab2[0].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				num = Double.parseDouble(Fuck.getText());
+				double a = Math.sinh(num);
+				Fuck.setText(Double.toString(a));
+			}
+		});
+		// cosh
+		ab2[3].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				num = Double.parseDouble(Fuck.getText());
+				double a = Math.cosh(num);
+				Fuck.setText(Double.toString(a));
+			}
+		});
+		// tanh
+		ab2[6].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				num = Double.parseDouble(Fuck.getText());
+				double a = Math.tanh(num);
+				Fuck.setText(Double.toString(a));
+			}
+		});
+		// x³
+		ab2[9].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				num = Double.parseDouble(Fuck.getText());
+				double a = Math.pow(num, 3);
+				String str = String.valueOf(a);
+				String[] data = str.split("\\.");
+				double n = Double.parseDouble(data[1]);
+				if (n == 0) {
+					Fuck.setText(data[0]);
+				} else {
+					Fuck.setText(Double.toString(a));
+				}
+			}
+		});
+		// log
+		ab2[10].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				num = Double.parseDouble(Fuck.getText());
+				double a = Math.log10(num);
+				String str = String.valueOf(a);
+				String[] data = str.split("\\.");
+				double n = Double.parseDouble(data[1]);
+				if (n == 0) {
+					Fuck.setText(data[0]);
+				} else {
+					Fuck.setText(Double.toString(a));
+				}
+			}
+		});
+		// x²
+		ab2[11].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				num = Double.parseDouble(Fuck.getText());
+				double a = Math.pow(num, 2);
+				String str = String.valueOf(a);
+				String[] data = str.split("\\.");
+				double n = Double.parseDouble(data[1]);
+				if (n == 0) {
+					Fuck.setText(data[0]);
+				} else {
+					Fuck.setText(Double.toString(a));
+				}
+			}
+		});
+		// =
+		ab[18].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				if (Fuck.getText().equals("")) {
+					abc = num;
+				} else {
+					abc = Double.parseDouble(Fuck.getText());
+				}
+
+				Double a = 0.0;
+				switch (op) {
+				case 1:
+					a = (num + abc);
+					break;
+				case 2:
+					a = (num - abc);
+					break;
+				case 3:
+					a = (num / abc);
+					break;
+				case 4:
+					a = (num * abc);
+					break;
+				default:
+					num = 0;
+				}
+				String str = String.valueOf(a);
+				String[] data = str.split("\\.");
+				double n = Double.parseDouble(data[1]);
+				if (n == 0) {
+					Fuck.setText(data[0]);
+				} else {
+					Fuck.setText(Double.toString(a));
+				}
+
+			}
+		});
+		// Fuckyou3_JButton放入Java4
+		for (int j = 4; j < 7; j++) {
+			ab[j].addActionListener(new Java4());
+		}
+		for (int k = 8; k < 11; k++) {
+			ab[k].addActionListener(new Java4());
+		}
+		for (int m = 12; m < 15; m++) {
+			ab[m].addActionListener(new Java4());
+		}
+		ab[16].addActionListener(new Java4());
+		ab[17].addActionListener(new Java4());
 	}
 
 	public static class Java4 implements ActionListener {
 		public void actionPerformed(ActionEvent ae) {
 			JButton asshole = (JButton) ae.getSource();
-			if (asshole.getText().equals("+") || asshole.getText().equals("-")
-					|| asshole.getText().equals("×")
-					|| asshole.getText().equals("÷")) {
-				Fuck.setText(Fuck.getText());
-			} else if (Fuck.getText().equals("0") && asshole.getText() != ".") {
+			if (Fuck.getText().equals("0") && asshole.getText() != ".") {
 				Fuck.setText("");
 				Fuck.setText(Fuck.getText() + asshole.getText());
 			} else {
 				Fuck.setText(Fuck.getText() + asshole.getText());
 			}
-
-			// String mustSplitString=Fuck.getText();
-			// String[] AfterSplit = mustSplitString.split("+ - × ÷");
-			long abc = 0;
-
-			if (asshole.getText().equals("+")) {
-				abc = Long.parseLong(Fuck.getText());
-				op = 1;
-				Fuck.setText(Fuck.getText());
-				
-//				Fuck.setText("");
-//				Fuck.setText(Fuck.getText() + asshole.getText());
-			} 
-			if (asshole == ab[15]) {
-				abc = Long.parseLong(Fuck.getText());
-				Fuck.setText(Fuck.getText());
-				op = 2;
-			}  
-			if (asshole == ab[11]) {
-				abc = Long.parseLong(Fuck.getText());
-				Fuck.setText(Fuck.getText());
-				op = 3;
-			} 
-			if (asshole == ab[7]) {
-				abc = Long.parseLong(Fuck.getText());
-				Fuck.setText(Fuck.getText());
-				op = 4;
-			} 
-			Fuck.setText("");
-			Fuck.setText(Fuck.getText() + asshole.getText());
-			if (asshole == ab[18]) {
-				num = Integer.parseInt(Fuck.getText());
-				switch (op) {
-
-				case 1:
-					num += abc;
-					break;
-				case 2:
-					num -= abc;
-					break;
-				case 3:
-					num *= abc;
-					break;
-				case 4:
-					num /= abc;
-					break;
-				default:
-					num = 0;
-				}
-				Fuck.setText(Long.toString(num));
-			}
 		}
-
 	}
-
 }
