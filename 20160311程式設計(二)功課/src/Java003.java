f20bb92419ba5a0a777711cad06f8ef60128a4a5
VizGS <104021020@live.asia.edu.tw>
2016-03-14 14:01:59 +0800

    JAVA

--- 20160311程式設計(二)功課/src/Java003.java
+++ 20160311程式設計(二)功課/src/Java003.java
@@ -12,78 +12,43 @@ public class Java003 {
 		int k = 0;
 		int f = a - 1;
 		for (int i = 0; i < 2 * a - 1; i++) {
-			if (a % 2 != 0) {
-				if (i < a) {
+			if (i < a) {
+				if (i % 2 != 0) {
+					j = 0;
+				}
+				for (int t = 0; t <= i; t++) {
+					k = i - j;
+					data[k][j] = count;
+					count++;
 					if (i % 2 != 0) {
-						j = 0;
+						j++;
+					} else {
+						j--;
 					}
-					for (int t = 0; t <= i; t++) {
-						k = i - j;
-						data[k][j] = count;
-						count++;
-						if (i % 2 != 0) {
-							j++;
-						} else {
-							j--;
-						}
-					}
-				} else {
-					for (int t = 0; t < f; t++) {
-						if (i % 2 == 0) {
-							k = i - j;
-						} else {
-							j = i - k;
-						}
-						data[k][j] = count;
-						count++;
-						if (i % 2 == 0) {
-							j--;
-						} else {
-							k--;
-						}
-					}
-					f--;
 				}
 			} else {
-				if (i < a) {
-					if (i % 2 != 0) {
-						j = 0;
-					}
-					for (int t = 0; t <= i; t++) {
-						k = i - j;
-						data[k][j] = count;
-						count++;
-						if (i % 2 != 0) {
-							j++;
-						} else {
-							j--;
-						}
-					}
+				if (i % 2 == 0) {
+					j = a - 1;
 				} else {
-					if (i % 2 == 0) {
-						j = a - 1;
-					} else {
-						k = a - 1;
-					}
-					for (int t = 0; t < f; t++) {
-						if (i % 2 == 0) {
-							k = i - j;
-						} else {
-							j = i - k;
-						}
-						data[k][j] = count;
-						count++;
-						if (i % 2 == 0) {
-							j--;
-						} else {
-							k--;
-						}
-					}
-					f--;
+					k = a - 1;
 				}
+				for (int t = 0; t < f; t++) {
+					if (i % 2 == 0) {
+						k = i - j;
+					} else {
+						j = i - k;
+					}
+					data[k][j] = count;
+					count++;
+					if (i % 2 == 0) {
+						j--;
+					} else {
+						k--;
+					}
+				}
+				f--;
 			}
 		}
-
 		for (int x = 0; x < a; x++) {
 			for (int y = 0; y < a; y++) {
 				System.out.print(data[x][y] + "\t");
